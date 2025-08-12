89c61dca1acdd9db533d85609e73cf1d64694731
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-08-12 23:51:45 +0300

    Add files via upload

---
+++ problem2.js
@@ -0,0 +1,9 @@
+// Check if the given variable is falsy and return "Invalid" if it is. =>
+var inputValue;
+function checkInputValue(inputVal){
+    if(!inputVal){
+        console.log("It Is Invalid Input Value");
+        return
+    }
+}
+console.log(checkInputValue(0))
