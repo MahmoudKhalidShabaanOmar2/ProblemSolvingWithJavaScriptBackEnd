a13b0352a1fd8090877857481319514d57e6473b
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-08-12 23:45:48 +0300

    Add files via upload

---
+++ problem1.js
@@ -0,0 +1,7 @@
+// Convert the string "123" to a number and add 7 =>
+var stringInput = "123";
+console.log("The String Input Is = ",stringInput);
+var number = Number(stringInput);
+console.log("The Number Is = ",number)
+var result = number + 7;
+console.log("The Result Is = ",result);
