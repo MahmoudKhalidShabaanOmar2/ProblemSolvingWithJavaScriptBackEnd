7f81647bd162ba42411bf69f1752dd585c421dbf
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-08-12 23:54:42 +0300

    Add files via upload

---
+++ problem3.js
@@ -0,0 +1,7 @@
+// Use for loop to print all numbers between 1 and 10, skipping even numbers using continue =>
+for ( var num=1 ; num <= 10 ; num++){
+    if(num %2 === 0){
+        continue
+    }
+    console.log(num)
+}
