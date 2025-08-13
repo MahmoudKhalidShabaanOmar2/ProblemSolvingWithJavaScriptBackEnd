6c1e94984e280eefa3ff16bda98ad70910b782dd
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-08-13 23:55:08 +0300

    Add files via upload

---
+++ problem9.js
@@ -0,0 +1,5 @@
+// Write a function using arrow syntax to return the square of a number =>
+var inputNumber = 5;
+console.log("The Input Number Is = ",inputNumber);
+const squareOfInputNumber = num => num * num;
+console.log("The Square Of The Input Number Is = ",squareOfInputNumber(inputNumber));
