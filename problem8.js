8d3b9689f47150e987af3cfd6aa1cf27610f9644
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-08-13 23:49:52 +0300

    Add files via upload

---
+++ problem8.js
@@ -0,0 +1,10 @@
+// Write a function that checks if a number is divisible by 3 and 5 =>
+var inputNumber = 15;
+console.log("The Number Is = ",inputNumber);
+console.log("Check If The Input Number Is Divisible By Three , And Five");
+if(inputNumber %3 === 0 && inputNumber %5 === 0){
+    console.log("It Is Divisible By Both Numbers");
+}
+else{
+    console.log("It Is Not Divisible By Both Numbers");
+};
