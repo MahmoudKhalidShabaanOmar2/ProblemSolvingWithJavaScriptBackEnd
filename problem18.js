0b395c4adcd94c0ca0a8bcb41cbe2a6faf3e57e7
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-08-15 23:55:19 +0300

    Add files via upload

---
+++ problem18.js
@@ -0,0 +1,5 @@
+// Write a function that splits a string into an array of words based on spaces. =>
+var stringInput = "The Quick Brown Fox";
+console.log("The String Input Is : ",stringInput);
+var arrayOfStringInput = stringInput.split(" ");
+console.log("The Array Of The String Text Is : ",arrayOfStringInput);
