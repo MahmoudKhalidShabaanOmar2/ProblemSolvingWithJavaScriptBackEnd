1eb2bab6b17d2e9d9102d16fa7452101547e5341
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-08-16 22:34:57 +0300

    Add files via upload

---
+++ problem20.js
@@ -0,0 +1,5 @@
+// Write a function that splits a string into an array of words based on underscoer =>
+const inputString = "The_Quick_Brwon_Fox";
+console.log("The Input String Is : ",inputString);
+const arrayOfStringInputs = inputString.split("_");
+console.log("The Array Of The String Inputs Is : ",arrayOfStringInputs);
