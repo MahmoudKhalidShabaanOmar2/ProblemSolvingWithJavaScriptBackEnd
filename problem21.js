3061efb4142233fc2148952cec52cd49d5a311c8
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-08-16 22:30:33 +0300

    Add files via upload

---
+++ problem21.js
@@ -0,0 +1,9 @@
+// write a java script function to get the modulus by two , and three of the input number =>
+var inputNumber = 8;
+function getModulusOfNumberBy2(num){
+    var modOfNumBy2 = num%2;
+    return modOfNumBy2;
+}
+console.log("The Input Number Is = ",inputNumber);
+var modOfInputNumber = getModulusOfNumberBy2(inputNumber);
+console.log("The Modulus Of The Input Number By Two Is = ",modOfInputNumber);
