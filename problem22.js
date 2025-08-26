6d2c9f2c04a2e142278e90384482bf532dfe8daa
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-08-26 22:11:57 +0300

    Add files via upload

---
+++ problem22.js
@@ -0,0 +1,6 @@
+// write a function to get the file path , and file directory by using java script =>
+function getFilePathAndFileDirectory(){
+    console.log("The File Path Is : ",__filename);
+    console.log("The File Directory Is : ",__dirname);
+};
+getFilePathAndFileDirectory();
