d7be0629164ec1b880d97e9a8ecda8dc1d1e14c0
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-08-27 23:33:30 +0300

    Add files via upload

---
+++ problem31.js
@@ -0,0 +1,5 @@
+// write a java script program to find the file path , and file directory from this point =>
+const filePath = __filename;
+console.log("The File Path Is : \"",filePath,"\"");
+const fileDirectory = __dirname;
+console.log("The File Directory Is : \"",fileDirectory,"\"");
