f42218a828cfe92d7092bafa8ef1bd48230ca4fb
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-09-02 23:34:01 +0300

    Add files via upload

---
+++ problem40.js
@@ -0,0 +1,5 @@
+// write a java script function to display hello wolrd and everyone from the nodejs by using arrow function method =>
+const displayHello = () =>{
+    console.log("Hello World , And EveryOne! From The NodeJS...");
+};
+displayHello();
