b1ad51428055bc62afef7a728e92f21e574b3394
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-09-02 23:38:25 +0300

    Add files via upload

---
+++ problem41.js
@@ -0,0 +1,10 @@
+// write a java script program to get the sum of the two numbers by using arrow function =>
+let fristNumber = 10;
+let secondNumber = 20;
+const sumOfTwoNumbers = (friNum , secNum) =>{
+    console.log("The Frist Number Is = ",friNum);
+    console.log("The Second Number Is = ",secNum);
+    const sumOfNumbers = friNum + secNum;
+    console.log("The Sum Of The Two Numbers Is = ",sumOfNumbers);
+}
+sumOfTwoNumbers(fristNumber , secondNumber);
