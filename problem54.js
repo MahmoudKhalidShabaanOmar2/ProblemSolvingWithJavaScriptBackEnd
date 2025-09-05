6f614fe3a6c37fe2895b2f33ac5a07b691a502c9
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-09-05 23:58:43 +0300

    Add files via upload

---
+++ problem54.js
@@ -0,0 +1,8 @@
+// write a java script program to get the sum of all numbers by using constant number =>
+const number = 5;
+console.log("The Number Is = ",number);
+let sumOfNumbers = 0;
+for( let i=0 ; i <= number ; i++){
+    sumOfNumbers += i;
+}
+console.log("The Sum Of All Numbers Is = ",sumOfNumbers);
