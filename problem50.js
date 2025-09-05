555059d1ac5e28505d4233d09ab2f4f0be4011b0
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-09-05 23:32:51 +0300

    Add files via upload

---
+++ problem50.js
@@ -0,0 +1,5 @@
+// write a java script program to get number , and then get the square root of the number =>
+let number = 25;
+console.log("The Number Is = ",number);
+let squareRootOfNumber = Math.sqrt(number);
+console.log(`The Square Root Of ${number} Is = ${squareRootOfNumber}`)
