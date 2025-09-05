5759c3b6bfa2cbb24f99874130235332eade9334
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-09-05 23:41:58 +0300

    Add files via upload

---
+++ problem51.js
@@ -0,0 +1,7 @@
+// write a java script prrogram to get the area of the traingle =>
+let traingleBaseValue = 20;
+console.log(`The Base Value Is = ${traingleBaseValue}`);
+let traingleHeightValue = 25;
+console.log(`The Height Value Is = ${traingleHeightValue}`);
+let traingleArea = (traingleBaseValue * traingleHeightValue) / 2;
+console.log(`The Area Of The Traingle Is = ${traingleArea} , Becausse Th Traingle Base Value Is = ${traingleBaseValue} , And The Traingle Height Value Is = ${traingleHeightValue}`);
