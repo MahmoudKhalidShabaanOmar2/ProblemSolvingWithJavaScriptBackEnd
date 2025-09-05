f77929a43a9169c05f3fd6580ece80baab88a75f
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-09-05 23:53:53 +0300

    Add files via upload

---
+++ problem53.js
@@ -0,0 +1,5 @@
+// write a java script program to convert from kilometers into miles =>
+let kilometers = 20;
+console.log("The Kilometers Is = ",kilometers,"KM");
+let miles = kilometers * 0.621371;
+console.log(`The Kilometers Is ${kilometers} KM To The Miles Is = ${miles} M.`)
