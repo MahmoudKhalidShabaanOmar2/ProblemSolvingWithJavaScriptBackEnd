3f4c858064612949d9bcce923d112fd8dc2f1b8d
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-09-06 23:35:59 +0300

    Add files via upload

---
+++ problem59.js
@@ -0,0 +1,5 @@
+// write a java script program to get the array of numbers , and then displaying all numbers inside array of numbers by using map array method =>
+let arrayOfNumbers = [1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10];
+console.log("The Array Of All Numbers Is : ")
+const arrOfNum = arrayOfNumbers.map((num) => (num));
+console.log(arrOfNum)
