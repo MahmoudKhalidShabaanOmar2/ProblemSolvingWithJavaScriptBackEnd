12ea237006dc577c62fb8a0e8b8a037812f3d5cb
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-09-21 21:44:02 +0300

    Add files via upload

---
+++ problem67.js
@@ -0,0 +1,7 @@
+// write a java script program to get the area of the traingle =>
+const baseOfTraingle = 25;
+console.log(`The Base Value Of The Traingle Is = ${baseOfTraingle}`);
+const heightOfTraingle = 20;
+console.log(`The Height Value Of The Traingle Is = ${heightOfTraingle}`);
+const areaOfTraingle = (baseOfTraingle * heightOfTraingle) / 2;
+console.log(`The Area Of The Traingle Is = ${areaOfTraingle}`);
