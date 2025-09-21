dafe07b6bf579d9708c69d0c558eade09fcd4feb
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-09-21 21:12:42 +0300

    Add files via upload

---
+++ problem62.js
@@ -0,0 +1,7 @@
+// write a java script program to print "Hello mahmoud!" to the user by using function =>
+const userName = "Mahmoud";
+function displayingHello(uName){
+    console.log("The User Name Is : ",uName);
+    console.log("hello ",uName,"!");
+}
+displayingHello(userName);
