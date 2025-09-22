101013b2c76e7e67666fd8eb269f8e870963ac07
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-09-22 23:04:08 +0300

    Add files via upload

---
+++ problem70.js
@@ -0,0 +1,12 @@
+// write a java script program to get two numbers from the user , and then swapping the two values of the numbers =>
+let fristNumber = 20;
+console.log("The Values Of The Two Numbers Before Swapping The Two Numbers Is : ")
+console.log("The Frist Number Is = ",fristNumber);
+let secondNumber = 40;
+console.log("The Second Number Is = ",secondNumber);
+let temporaryValue = fristNumber;
+fristNumber = secondNumber;
+secondNumber = temporaryValue;
+console.log("The Values Of The Two Numbers After Swapping The Two Numbers Is : ");
+console.log("The Frist Number Is = ",fristNumber);
+console.log("The Second Number Is = ",secondNumber);
