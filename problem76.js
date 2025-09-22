2565384f74ce1d50a189b4753861a8e64893fe53
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-09-22 23:41:51 +0300

    Add files via upload

---
+++ problem76.js
@@ -0,0 +1,9 @@
+// write a java script program to get number from the user , and then check if the number is even , or odd number =>
+const number = 25;
+console.log("The Number Is = ",number);
+if(number %2 == 0){
+    console.log(`The Number Is Even Number , Because Your Entered Number Is = ${number}`);
+}
+else{
+    console.log(`The Number Is Odd Number , Because Your Entered Number Is = ${number}`);
+};
