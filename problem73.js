730218cc3ec0927437b967e404592dba5147b127
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-09-22 23:26:51 +0300

    Add files via upload

---
+++ problem73.js
@@ -0,0 +1,12 @@
+// write a java script program to get number from the user , then check if the number is positive , zero , or negative number =>
+number = -89;
+console.log("The Number Is = ",number);
+if(number > 0){
+    console.log(`The Number Is Positive , Because Your Entered Number Is = ${number}`)
+}
+else if(number == 0){
+    console.log(`The Number Is Equal To Zero , Because Your Enteres Number Is = ${number}`)
+}
+else{
+    console.log(`The Number Is Negative , Because Your Entered Number Is = ${number}`)
+}
