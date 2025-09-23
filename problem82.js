58d8536a40b6352c0b38c90684b433d064ff09ac
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-09-23 23:44:37 +0300

    Add files via upload

---
+++ problem82.js
@@ -0,0 +1,7 @@
+// write a java script program to get number , and then get the factorial of number =>
+let number = 5;
+let factorial = 1;
+for (var i=1 ; i <= number ; i++){
+    factorial = factorial * i
+}
+console.log(`The Factorial Of ${number} Is = ${factorial}`);
