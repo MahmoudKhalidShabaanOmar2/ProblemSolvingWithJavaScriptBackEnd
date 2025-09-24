65ca874fa55dbbfe78616ae1bb9fb33c779fc16f
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-09-24 23:58:53 +0300

    Add files via upload

---
+++ problem88.js
@@ -0,0 +1,5 @@
+// write a java script program to get string , and then replace the string =>
+let string = "Hello Mahmoud";
+console.log("The String Is : ",string);
+let updatedString = string.replace("Mahmoud" , "Ahmed");
+console.log("The Updated String Is : ",updatedString)
