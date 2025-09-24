65ddfa9322b25745de3a52fa25ccf7de86abc470
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-09-24 23:45:47 +0300

    Add files via upload

---
+++ problem85.js
@@ -0,0 +1,8 @@
+// write a java script program to get the sum of all natural number =>
+const number = 10;
+console.log(`The Number Is ${number}`);
+let sumOfAllNaturalNumber = 0;
+for( var i=0 ; i < number ; i++){
+    sumOfAllNaturalNumber += i;
+}
+console.log(`The Sum Of All Natural Numbers Is = ${sumOfAllNaturalNumber}`);
